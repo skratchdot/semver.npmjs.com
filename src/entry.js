d0d3dee93e76288f5126bb1f8e824a3b9be8b7ea
◬ <git@skratchdot.com>
2015-04-09 02:41:00 -0400

    Permalink option. Fixes #3

--- src/entry.js
+++ src/entry.js
@@ -6,9 +6,27 @@ var semver = require('semver');
 
 var REGISTRY_CORS_PROXY = 'https://cors-proxy-ee2bb0df.internal.npmjs.com';
 
-app.controller('VersionCtrl', function($scope, $http) {
+var getPackageAndRange = function ($scope, $location) {
+  var searchObject = $location.search();
+  $scope.package = searchObject.package || 'lodash';
+  $scope.range = searchObject.range || '1.x';
+};
+
+var setPackageAndRange = function ($scope, $location) {
+  $location.search('package', $scope.package);
+  $location.search('range', $scope.range);
+  $location.replace();
+};
+
+app.run(function ($rootScope) {
+    $rootScope.$on('$locationChangeSuccess', function ($scope, $location) {
+      getPackageAndRange($scope, $location);
+    });
+});
+
+app.controller('VersionCtrl', function($scope, $http, $location) {
   var versions;
-  $scope.package = 'lodash';
+  getPackageAndRange($scope, $location);
 
   $scope.getVersions = function() {
     $scope.loading = true;
@@ -16,7 +34,6 @@ app.controller('VersionCtrl', function($scope, $http) {
       .success(function(data, status, headers, config) {
         versions = Object.keys(data.versions);
 
-        $scope.range = '1.x'
         $scope.versions = versions.map(function(v) {
           return {
             "version": v
@@ -27,6 +44,7 @@ app.controller('VersionCtrl', function($scope, $http) {
           for (var i=0, len=versions.length; i<len; i++) {
             $scope.versions[i].satisfies = semver.satisfies($scope.versions[i].version, $scope.range);
           }
+          setPackageAndRange($scope, $location);
         }
 
         $scope.checkVersions();
@@ -34,6 +52,7 @@ app.controller('VersionCtrl', function($scope, $http) {
       })
       .error(function(data, status, headers, config) {
         $scope.loading = false;
+        setPackageAndRange($scope, $location);
 
         console.log('Sorry, could not load data.')
       });
